7cfed21ca75683dcde2f0fb3c1a921c5dd25919a
FarooqAbdullah <farooq14162@gmail.com>
2015-07-28 11:51:21 +0500

    minor changes

---
+++ js/theme.js
@@ -0,0 +1,35 @@
+jQuery(document).ready(
+    function ($) {
+
+        $(function(){
+            var $class, $className;
+            $('.dropdown').hover(function() {
+                    $(this).addClass('open');
+                    $(this).css('position','static');
+                },
+                function() {
+                    $(this).removeClass('open');
+                    $(this).css('position','relative');
+                });
+        });
+
+        // navigation click actions
+        $('.dropdown-menu > ul > li > a').on('click', function(event ){
+            event.preventDefault();
+            var sectionID = $(this).attr("data-id");
+            //console.log($id);
+            //var sectionID = "americano";
+            scrollToID('#' + sectionID, 1000);
+        });
+        // scroll function
+        function scrollToID(id, speed){
+
+            var offSet = 50;
+            var targetOffset = $(id).offset().top - offSet;
+            $('html,body').animate({scrollTop:targetOffset}, speed);
+
+        }
+    }
+);
+
+
